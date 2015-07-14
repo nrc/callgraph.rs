0cd8a2ca499bc2b6ff6c519caa607bcbf35ccec1
Nick Cameron <ncameron@mozilla.com>
2015-07-14 22:01:57 +1200

    Static method calls

--- src/visitor.rs
+++ src/visitor.rs
@@ -13,7 +13,7 @@ use graphviz::{self, Labeller, GraphWalk};
 use rustc::middle::ty;
 use rustc_trans::save::{self, SaveContext};
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::iter::FromIterator;
 
@@ -23,7 +23,7 @@ use syntax::{ast, visit};
 pub struct RecordVisitor<'l, 'tcx: 'l> {
     save_cx: SaveContext<'l, 'tcx>,
 
-    static_calls: HashMap<NodeId, NodeId>,
+    static_calls: HashSet<(NodeId, NodeId)>,
     dynamic_calls: HashMap<NodeId, NodeId>,
     functions: HashMap<NodeId, String>,
     method_decls: HashMap<NodeId, String>,
@@ -36,7 +36,7 @@ impl<'l, 'tcx: 'l> RecordVisitor<'l, 'tcx> {
         RecordVisitor {
             save_cx: SaveContext::new(tcx),
 
-            static_calls: HashMap::new(),
+            static_calls: HashSet::new(),
             dynamic_calls: HashMap::new(),
             functions: HashMap::new(),
             method_decls: HashMap::new(),
@@ -57,7 +57,7 @@ impl<'l, 'tcx: 'l> RecordVisitor<'l, 'tcx> {
         }
 
         println!("\nFound calls:");
-        for (from, to) in self.static_calls.iter() {
+        for &(ref from, ref to) in self.static_calls.iter() {
             let from = &self.functions[from];
             let to = &self.functions[to];
             println!("{} -> {}", from, to);
@@ -70,40 +70,77 @@ impl<'l, 'tcx: 'l> RecordVisitor<'l, 'tcx> {
         let mut file = File::create("out.dot").unwrap();
         graphviz::render(self, &mut file).unwrap();
     }
-}
 
-impl<'v, 'l, 'tcx: 'l> visit::Visitor<'v> for RecordVisitor<'l, 'tcx> {
-    fn visit_expr(&mut self, ex: &'v ast::Expr) {
-        if save::generated_code(ex.span) {
+    fn record_method_call(&mut self, mrd: &save::MethodCallData) {
+        if self.cur_fn.is_none() {
+            println!("WARNING: call at {:?} without known current function",
+                     mrd.span);
             return;
         }
 
+        if let Some(ref_id) = mrd.ref_id {
+            if ref_id.krate == ast::LOCAL_CRATE {
+                self.static_calls.insert((self.cur_fn.unwrap(), ref_id.node));
+            }
+            return;
+        }
 
-
-        visit::walk_expr(self, ex)
+        if let Some(decl_id) = mrd.decl_id {
+            if decl_id.krate == ast::LOCAL_CRATE {
+                // TODO dynamic calls
+            }
+        }
     }
+}
 
+impl<'v, 'l, 'tcx: 'l> visit::Visitor<'v> for RecordVisitor<'l, 'tcx> {
     fn visit_path(&mut self, path: &'v ast::Path, id: NodeId) {
         if save::generated_code(path.span) {
             return;
         }
 
         let data = self.save_cx.get_path_data(id, path);
-        if let save::Data::FunctionCallData(frd) = data {
-            if frd.ref_id.krate == ast::LOCAL_CRATE {
-                let to = frd.ref_id.node;
+        // TODO need to check for method calls too
+        if let save::Data::FunctionCallData(ref fcd) = data {
+            if fcd.ref_id.krate == ast::LOCAL_CRATE {
+                let to = fcd.ref_id.node;
                 if let Some(from) = self.cur_fn {
-                    self.static_calls.insert(from, to);
+                    self.static_calls.insert((from, to));
                 } else {
                     println!("WARNING: call at {:?} without known current function",
-                             frd.span);
+                             fcd.span);
                 }
             }
+        }
+        if let save::Data::MethodCallData(ref mrd) = data {
+            self.record_method_call(mrd);
         }
 
         visit::walk_path(self, path)
     }
 
+    fn visit_expr(&mut self, ex: &'v ast::Expr) {
+        if save::generated_code(ex.span) {
+            return;
+        }
+
+        visit::walk_expr(self, ex);
+
+        match ex.node {
+            // ast::Expr_::ExprCall(_) => {
+
+            // }
+            ast::Expr_::ExprMethodCall(..) => {}
+            _ => {
+                return;
+            }
+        }
+
+        let data = self.save_cx.get_expr_data(ex);
+        if let Some(save::Data::MethodCallData(ref mrd)) = data {
+            self.record_method_call(mrd);
+        }
+    }
 
     fn visit_item(&mut self, item: &'v ast::Item) {
         if save::generated_code(item.span) {
@@ -128,6 +165,10 @@ impl<'v, 'l, 'tcx: 'l> visit::Visitor<'v> for RecordVisitor<'l, 'tcx> {
     }
 
     fn visit_trait_item(&mut self, ti: &'v ast::TraitItem) {
+        if save::generated_code(ti.span) {
+            return;
+        }
+
         match ti.node {
             ast::TraitItem_::MethodTraitItem(_, None) => {
                 let fd = self.save_cx.get_method_data(ti.id, ti.ident.name, ti.span);
@@ -152,19 +193,20 @@ impl<'v, 'l, 'tcx: 'l> visit::Visitor<'v> for RecordVisitor<'l, 'tcx> {
     }
 
     fn visit_impl_item(&mut self, ii: &'v ast::ImplItem) {
-        match ii.node {
-            ast::ImplItem_::MethodImplItem(..) => {
-                let fd = self.save_cx.get_method_data(ii.id, ii.ident.name, ii.span);
-                self.functions.insert(fd.id, fd.qualname);
+        if save::generated_code(ii.span) {
+            return;
+        }
 
-                let prev_fn = self.cur_fn;
-                self.cur_fn = Some(fd.id);
-                visit::walk_impl_item(self, ii);
-                self.cur_fn = prev_fn;
+        if let ast::ImplItem_::MethodImplItem(..) = ii.node {
+            let fd = self.save_cx.get_method_data(ii.id, ii.ident.name, ii.span);
+            self.functions.insert(fd.id, fd.qualname);
 
-                return;
-            }
-            _ => {}
+            let prev_fn = self.cur_fn;
+            self.cur_fn = Some(fd.id);
+            visit::walk_impl_item(self, ii);
+            self.cur_fn = prev_fn;
+
+            return;
         }
 
         visit::walk_impl_item(self, ii)
@@ -193,7 +235,8 @@ impl<'a, 'l, 'tcx: 'l> GraphWalk<'a, NodeId, Edge> for RecordVisitor<'l, 'tcx> {
     }
 
     fn edges(&'a self) -> graphviz::Edges<'a, Edge> {
-        graphviz::Edges::from_iter(self.static_calls.iter().map(|(f, t)| (f.clone(), t.clone())))
+        graphviz::Edges::from_iter(
+            self.static_calls.iter().map(|&(ref f, ref t)| (f.clone(), t.clone())))
     }
 
     fn source(&'a self, &(from, _): &Edge) -> NodeId {
